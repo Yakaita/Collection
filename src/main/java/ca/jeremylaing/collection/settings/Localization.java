d89046dd7bd369b288977db6ebe07da8043d7dc9
Jeremy <jeremy.r.laing@gmail.com>
2021-10-01 19:41:18 -0400

    started locale stuff. made settings a little pretty

---
+++ src/main/java/ca/jeremylaing/collection/settings/Localization.java
@@ -0,0 +1,15 @@
+package ca.jeremylaing.collection.settings;
+
+import ca.tweetzy.tweety.settings.SimpleLocalization;
+
+public final class Localization extends SimpleLocalization {
+
+
+
+
+
+    @Override
+    protected int getConfigVersion() {
+        return 1;
+    }
+}
